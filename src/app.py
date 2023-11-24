4e6d07cedb12317847a47a468bbfcffcd9ced694
Ashi Friedberg <ashifriedberg@gmail.com>
2023-11-24 04:16:09 +0200

    need to figure out block type, JSON/dict

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import json
 from time import time
 from hashlib import sha256
+from flask import Flask, jsonify
+import requests
 
 
 class Blockchain():
@@ -10,6 +12,7 @@ class Blockchain():
         self.chain = []
         self.new_transactions = []
         self.nodes = []
+        self.new_block(1, "0")
 
 
     def new_block(self, nonce, previous_hash):
@@ -24,10 +27,11 @@ class Blockchain():
             "content": self.new_transactions
 
         }
-        block_json = json.dumps(block)
-        self.chain.append(block_json)
+        block_json = json.dumps(block, sort_keys=True)
+        self.chain.append(block)
         # Clear new transactions list
         self.new_transactions = []
+    
 
 
     
@@ -47,7 +51,7 @@ class Blockchain():
     def hash_block(self, block):
         """Hashes the contents of the block"""
 
-        block_content = json.loads(block)
+        block_content = json.dumps(block, sort_keys=True).encode()
         return sha256(block_content).hexdigest()
     
 
@@ -59,22 +63,24 @@ class Blockchain():
 
     def hash_nonces(self, nonce, previous_nonce):
         """returns the hash of both nonces"""
-        sum = nonce + previous_nonce
-        return sha256(sum).hexdigest()
+        sum = str(nonce + previous_nonce)
+        noth = sum.encode()
+        return sha256(noth).hexdigest()
     
 
 
     def proof_of_work(self, previous_nonce):
         """Requires the hash of both nonces to begin with 4 0's"""
         nonce = 0
-        while self.hash_nonces(nonce, previous_nonce)[0:3] != "0000":
+        difficulty = 3
+        while self.hash_nonces(nonce, previous_nonce)[1] != "0":
             nonce += 1
         return nonce
     
 
     def valid_chain(self, chain):
         """Loop through chain, check validity based on hashes and POW"""
-        for i in range(len(chain), 0, -1):
+        for i in range(len(chain) - 1, 0, -1):
             if chain[i]["previous_hash"] != sha256(chain[i - 1]).hexdigest():
                 return False
             elif self.hash_nonces(chain[i]["nonce"], chain[i - 1]["nonce"])[0:3] != "0000":
@@ -85,7 +91,29 @@ class Blockchain():
 
     def resolve_conflicts(self):
         """Replaces local chain with the longest chain on the network"""
-        #for node in self.nodes:
+        local_length = len(self.chain)
+        for node in self.nodes:
+            response = requests.get(f"{node}/chain")
+            if response.status_code == "200":
+                other_chain_length = response.json()["length"]
+                other_chain = response.json()["chain"]
+                if int(other_chain_length) > local_length:
+                    self.chain = json.loads(other_chain)
+
+
+
+
+# blockchain = Blockchain()
+
+# blockchain.new_transaction("me", "you", "hello")
+
+# blockchain.new_block(blockchain.proof_of_work(1), blockchain.hash_block(blockchain.last_block()))
+
+# blockchain.new_transaction("alice", "bob", "open-secret")
+
+# blockchain.new_block(60, "8a792b3269349f5ffa6d40e3c8a4232ff8282b117432de0eb95df5fb78e60818")
+
+# print(blockchain.chain[1]["previous_hash"])
 
 
 
